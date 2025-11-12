fd86a4646b55fb86330ea9da8fa23f2542d2f155
Tarkile <89094931+Tarkile@users.noreply.github.com>
2025-11-12 11:02:56 -0500

    commit

--- calculator.py
+++ calculator.py
@@ -4,9 +4,9 @@ calculator.py
 
 One function per operation, in order.
 """
-#https://github.com/Tarkile/lab11--SF---LLC-
-#partner 1: Samuel Finlinson
-#partner 2: Leonardo Lopez-Casula
+# https://github.com/Tarkile/lab11--SF---LLC-
+# partner 1: Samuel Finlinson
+# partner 2: Leonardo Lopez-Casula
 
 import math
 
