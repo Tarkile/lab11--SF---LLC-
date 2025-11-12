330cce7b73dc5d035f8fb370dd908e941eb34f99
Tarkile <89094931+Tarkile@users.noreply.github.com>
2025-11-12 10:25:09 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -4,6 +4,29 @@ calculator.py
 
 One function per operation, in order.
 """
+import math
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return a / b
+
+def log(a, b):
+    if a <= 0 or b <= 0 or b == 1:
+        raise ValueError("Logarithm inputs must be positive and base cannot be 1")
+    return math.log(a, b)
+
+def exp(a, b):
+    return a ** b
 
 
 
